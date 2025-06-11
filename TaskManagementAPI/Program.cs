5317b3269cba1a785836995dbf150d419f9d010b
Sheron Makola <sheron@synthesis.co.za>
2025-06-11 19:45:08 +0200

    confugured https and swagger

--- TaskManagementAPI/Program.cs
+++ TaskManagementAPI/Program.cs
@@ -1,15 +1,20 @@
+using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Task Management API", Version = "v1" });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
--- TaskManagementAPI/obj/Release/net9.0/TaskManagementAPI.AssemblyInfo.cs
+++ TaskManagementAPI/obj/Release/net9.0/TaskManagementAPI.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagementAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57abd0548c97315b585db9ae6f569d855cb8b458")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagementAPI")]
